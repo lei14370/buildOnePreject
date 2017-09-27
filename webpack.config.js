d90d15e6b2c3b9eb035b6cbaa0296cfe3e1c92aa
lileilei <lileilei@PC201505271027>
2017-09-27 15:45:25 +0800

    你好

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+module.exports={
+    entry:'./src/script/main.js',
+    output:{
+        path:path.resolve(__dirname, "dist"),
+        filename:'bundle.js'
+    }
+
+}
